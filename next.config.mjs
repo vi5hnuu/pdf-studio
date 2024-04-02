d4a31f87adb6f006e8a7910e5fa728889a8e8f4f
vi5hnu <kumarvishnu1619@gmail.com>
2024-04-02 21:42:30 +0530

    output export in nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
     webpack: (config) => {
         config.resolve.alias.canvas = false;
         return config;
