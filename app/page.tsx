56ae0c89f2080e80772b3d4968131073d3d265ba
vi5hnu <kumarvishnu1619@gmail.com>
2024-04-07 17:44:15 +0530

    main page css changed

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
     return (
         <main className="min-h-screen flex-col items-center justify-start p-24">
             <h1 className='mx-auto text-6xl mb-6 w-full text-center'>Pdf Studio</h1>
-            <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6'>
+            <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
                 {tools.map((tool, index) => <Link key={index} href={toolsInfo[tool].path}><Card
-                                                                                    className='mx-auto !bg-green-200 w-72 h-60 !rounded-md !shadow-sm hover:!shadow-md transition-all cursor-pointer'>
+                                                                                    className='mx-auto !bg-green-200 w-64 h-60 !rounded-md !shadow-sm hover:!shadow-md transition-all cursor-pointer'>
                     <CardContent className='flex items-center justify-center h-full text-3xl font-normal'>
                         {toolsInfo[tool].title}
                     </CardContent>
