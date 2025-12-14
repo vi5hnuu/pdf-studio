463c4aa01f96c11d2e2db0d77e272f23eb2deee1
vishnu kumar <kumarvishnu1619@gmail.com>
2025-12-14 14:30:56 +0530

    height set to dyanamidynamic

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import {toolsInfo} from "@/app/_utils/constants";
 
 
 export default function Home() {
-    return (<main className="flex flex-col overflow-auto h-screen bg-gradient-to-br from-slate-50 to-slate-100">
+    return (<main className="flex flex-col overflow-auto h-dvh bg-gradient-to-br from-slate-50 to-slate-100">
         <header className="bg-white shadow-sm border-b border-slate-200">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
                 <div className="flex items-center gap-3">
