013080886b2342309e61526cbcebf246819d6baf
vishnu kumar <kumarvishnu1619@gmail.com>
2026-03-04 13:41:06 +0530

    bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
@@ -9,11 +7,20 @@ const inter = Inter({ subsets: ["latin"] });
 //react-pdf
 import { pdfjs } from 'react-pdf';
 import Script from "next/script";
+import {Metadata} from "next";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.js',
     import.meta.url,
 ).toString();
+
+export const metadata: Metadata = {
+    title: "Pdf Studio",
+    description: "Pdf Studio by Laxmi Solutions",
+    other: {
+        "google-adsense-account": "ca-pub-4715945578201106"
+    }
+};
 
 export default function RootLayout({
   children,
